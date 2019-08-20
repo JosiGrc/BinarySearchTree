eb7f4ce0ec5bf3f972d59ee01f62305d298a75cd
Josi <JosiGrc@users.noreply.github.com>
2019-08-20 10:24:03 -0500

    Add method completed, starting on Search method

--- BinarySearchTree/BinarySearchTree/SearchTree.cs
+++ BinarySearchTree/BinarySearchTree/SearchTree.cs
@@ -10,47 +10,63 @@ namespace BinarySearchTree
     {
         //Variables
         Node start;
-        
+
 
         //Ctor
         public SearchTree()
         {
-                                                         
+
         }
 
         //Methods
-        public void InsertNewNode (int data)
+        public void AddNewNode(int data)
         {
             //makes a new node for the root node
             Node nodeToAdd = new Node(data);
-            
-                
-            if (start == null)                   
-            {                                                                   
+
+
+            if (start == null)
+            {
                 start = nodeToAdd;
                 return;
             }
 
             Node node = start;
-            //check if theres a node there, if nor make one
+            //check if theres a node there, if nor make one          
 
-            if (nodeToAdd == null)
+            while (true)
             {
-                nodeToAdd = new Node(data);     //checks if theres a node, if not one gets made
-            }
-
-            while(true)                                     
-            {                
-                if(data > node.data)
+                if (data > node.data)
                 {
-                    node.rightChild = nodeToAdd;                  //make this part a loop for everytime a new value is added  
+                    if (node == null)
+                    {
+                        nodeToAdd = new Node(data);
+                        node.rightChild = new Node(data);
+                    }
+                    else
+                    {
+                        node = node.rightChild;
+                    }
                 }
                 else if (data <= node.data)
                 {
-                    node.leftChild = nodeToAdd;
-                }
+                    if (node == null)
+                    {
+                        nodeToAdd = new Node(data);
+                        node.leftChild = new Node(data);
+                    }
+                    else
+                    {
+                        node = node.leftChild;
+                    }
 
-            }           
+                }
+            }
         }
+
+        //public int SearchNode()
+        //{
+
+        //}
     }
 }
